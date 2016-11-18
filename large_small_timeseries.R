c2fe0658a8f28e0e011ffe12cb772b8868457d9c
Abolfazl <abolfazl@ufl.edu>
2016-11-18 03:04:36 -0500

    add function to determine size class get species into size classes whether >50 or <50

--- large_small_timeseries.R
+++ large_small_timeseries.R
@@ -6,3 +6,12 @@ get_data <- function(){
 add_data <- function(){
   data <- read.csv ("surveys.csv")
 }
+get_size <- function(weight){
+  if ((weight)>50){
+    size_class="large"
+  }
+  else{
+    size_class="small"
+  }
+  return(size_class)
+}
